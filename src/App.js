711fb1741f6ed30ad3d85e9f1680c413b30f0cc1
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-10-28 14:33:42 -0400

    add stuff to my main component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useCallback, useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddBook from "./components/AddBook";
+import BookList from "./components/BookList";
+import Header from "./components/Header";
+import BorrowedBooks from "./components/BorrowedBooks";
+// import Conditionalbuttons from "./components/ConditionalButtons";
+import "./App.css";
 function App() {
+  const [books, setBooks] = useState([]);
+  const [user, setUser] = useState("guest");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/books")
+      .then((response) => response.json())
+      .then((data) => setBooks(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, [refresh]);
+
+  useCallback(refresh, [refresh]);
+  function refresh() {
+    return null;
+  }
+
+  const handleDeleteBook = async (bookId) => {
+    const response = await fetch(`http://localhost:3001/books/${bookId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Router>
+        <Header
+          onSetUser={setUser}
+          isLoggedIn={isLoggedIn}
+          onSetIsLoggedIn={setIsLoggedIn}
+          refresh={refresh}
+        />
+        {/* <BorrowedBooks user={user} isloggedIn={isLoggedIn} Books={books} /> */}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <BookList
+                books={books}
+                setBooks={setBooks}
+                isLoggedIn={isLoggedIn}
+                user={user}
+                handleDeleteBook={handleDeleteBook}
+              />
+            }
+          />
+
+          {/* <Route path="/" element={<Conditionalbuttons  />} /> */}
+
+          <Route
+            path="/admin"
+            element={
+              <BookList
+                books={books}
+                setBooks={setBooks}
+                isLoggedIn={isLoggedIn}
+                user={user}
+                handleDeleteBook={handleDeleteBook}
+              />
+            }
+          />
+
+          <Route path="/AddBook" element={<AddBook onRefresh={refresh} />} />
+        </Routes>
+      </Router>
+      {/* <BookList
+        books={books}
+        setBooks={setBooks}
+        isLoggedIn={isLoggedIn}
+        user={user}
+        handleDeleteBook={handleDeleteBook}
+      /> */}
+      {/* {user === "admin" ? <AddBook Setbooks={refresh} /> : null}; */}
     </div>
   );
 }
